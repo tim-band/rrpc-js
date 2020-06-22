fe20e53d881429bf9a0e9b32112af3a469566d81
Tim Band <t.band>
2020-06-22 18:58:34 +0100

    Initial commit

---
+++ rrpc.js
@@ -0,0 +1,85 @@
+window.rrpc = function () {
+
+  var web_socket = null;
+  var callbacks = {};
+  var currentId = 1;
+  const jsonrpc = "2.0";
+  var initialErrorCallback = null;
+
+  function nextId() {
+    const id = currentId;
+    currentId++;
+    return id;
+  }
+
+  function tearDown() {
+    if (web_socket) {
+      web_socket.close();
+    }
+    const oldCallbacks = callbacks;
+    callbacks = {};
+    for (c in oldCallbacks) {
+      oldCallbacks[c](null, new Error('WebSocket torn down'));
+    }
+  }
+
+  function processMessage(event) {
+    const data = JSON.parse(event.data);
+    if (data.id in callbacks) {
+      const cb = callbacks[data.id];
+      delete callbacks[data.id];
+      cb(data.result, data.error);
+    }
+  }
+
+  function initializeWebSocket(openCallback, errorCallback, host) {
+    web_socket = new WebSocket("ws://" + host + "/websocket");
+    if (initialErrorCallback) {
+      web_socket.onerror = errorCallback;
+    }
+    web_socket.onopen = function () {
+      web_socket.onmessage = processMessage;
+      web_socket.onclose = function (event) {
+        tearDown();
+        // try to re-connect
+        initializeWebSocket(openCallback,
+          function (event) {
+            // If we are trying to re-connect because the other end has gone down,
+            // we just have to die silently.
+            if (event.error.code !== "ECONNREFUSED") {
+              initialErrorCallback(event.error);
+            }
+          },
+          host);
+      }
+      if (openCallback) {
+        openCallback();
+      }
+    }
+  }
+
+  return {
+
+    initialize: function (openCallback=null,
+      errorCallback=null,
+      host=window.location.host) {
+      tearDown();
+      initialErrorCallback = errorCallback;
+      initializeWebSocket(openCallback, errorCallback, host);
+    },
+
+    call: function (method, params, callback) {
+      const id = nextId();
+      if (callback) {
+        callbacks[id] = callback;
+      }
+      web_socket.send(JSON.stringify({ jsonrpc, method, params, id }));
+    },
+
+    destroy: function (callback) {
+      tearDown();
+    },
+
+  }
+
+}();
---
+++ test/selenium.js
@@ -0,0 +1,76 @@
+"use strict";
+
+const wsDomain = "127.0.0.1"
+const wsPort = 50056;
+const wsHost = wsDomain + ":" + wsPort;
+const jsonrpc = "2.0";
+
+const { spawn } = require('child_process');
+const { describe, before, after, it } = require('mocha');
+const assert = require("assert");
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
+describe('rrpc', function () {
+  let rProcess;
+  let driver;
+
+  before(function () {
+    rProcess = spawn('Rscript', ['test/server.R']);
+    driver = new Builder().forBrowser('firefox').build();
+  });
+
+  after(async function () {
+    // this function is async to make Mocha wait until
+    // all the tests have resolved before it calls it
+    rProcess.kill('SIGHUP');
+    driver.quit();
+  })
+
+  it('calls R functions', async function () {
+    this.timeout(10000);
+    await driver.get('http://127.0.0.1:50056/test/test.html');
+    await setXY(driver, 17, 45);
+    await clickButton(driver, "add");
+    await awaitResult(driver, "62");
+    await clickButton(driver, "broken");
+    await setXY(driver, 15, 7);
+    await clickButton(driver, "multiply");
+    await awaitResult(driver, "105");
+  });
+});
+
+async function setXY(driver, x, y) {
+  await reset(driver);
+  await xInput(driver, x);
+  await yInput(driver, y);
+}
+
+async function clickButton(driver, id) {
+  await driver.findElement(By.id(id)).click();
+}
+
+async function xInput(driver, value) {
+  const id = "x-input";
+  await input(driver, id, value);
+}
+
+async function yInput(driver, value) {
+  const id = "y-input";
+  await input(driver, id, value);
+}
+
+async function input(driver, id, value) {
+  await driver.findElement(By.id(id)).sendKeys("" + value);
+}
+
+async function reset(driver) {
+  await driver.findElement(By.id("reset")).click();
+}
+
+async function awaitResult(driver, value) {
+  const element = driver.findElement(By.id("result"));
+  await driver.wait(async function() {
+    const elementValue = await element.getAttribute("value");
+    return elementValue === value;
+  }, 10000);
+}
